9488c79889bd2a2f0f033957049353868d51ec96
Dylan <dfryed@gmail.com>
2018-07-11 10:47:19 -0500

    webpack compiles successfully

--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,11 @@
-console.log('Change?');
+import dotenv from 'dotenv';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+dotenv.config(); //keep keys and values safe in '.env'
+
+ReactDOM.render(<div><p>Hello</p></div>, document.getElementById('app'));
+
+module.hot.accept();
