2d03c98ecf6613b9c857285f9009a743317c773b
Dylan <dfryed@gmail.com>
2018-07-16 14:18:08 -0500

    Switched files to use routing and users from previously built api

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import New from './New';
 import { BrowserRouter } from 'react-router-dom';
 
 dotenv.config(); //keep keys and values safe in '.env'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
+ReactDOM.render(<BrowserRouter><New /></BrowserRouter>, document.getElementById('app'));
 
 module.hot.accept();
