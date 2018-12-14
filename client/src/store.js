ee25810f8a6c840a2e5c34ddacab5245cfc13d15
Dylan Frye <dylanfrye@Dylans-iMac.fios-router.home>
2018-12-14 14:49:39 -0600

    Testing tests.

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,12 @@ const middleware = [thunk];
 let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
 window.__REDUX_DEVTOOLS_EXTENSION__();
 
-if(process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+//compose() with devTools only runs in development
+if(
+  process.env.NODE_ENV === 'prod' ||
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'test'
+) {
     devTools = a => a;
 }
 
